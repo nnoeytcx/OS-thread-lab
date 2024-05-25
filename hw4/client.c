ee5142ce1f3c533dd0e64897319219756ddfe5a8
nnoeytcx <thitichaya5422@gmail.com>
2024-05-25 21:39:20 +0700

    done hw4

--- hw4/client.c
+++ hw4/client.c
@@ -1,63 +1,122 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h> 
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[])
+#define PORT 12056
+#define PORT2 22056
+#define BUFFER_SIZE 1024
+
+int port2()
 {
-    int sockfd = 0, n = 0;
-    char recvBuff[1024];
-    struct sockaddr_in serv_addr; 
+    int sock = 0;
+    char buffer[BUFFER_SIZE] = {0};
+    char *bye_mes = "Bye";
+    struct sockaddr_in serv_addr;
 
-    if(argc != 2)
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
-        return 1;
-    } 
-
-    memset(recvBuff, '0',sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    } 
-
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
+        printf("\n Socket creation error \n");
+        return -1;
+    }
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000); 
+    serv_addr.sin_port = htons(PORT2);
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
     {
-        printf("\n inet_pton error occured\n");
-        return 1;
-    } 
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-       printf("\n Error : Connect Failed \n");
-       return 1;
-    } 
+        printf("\nConnection Failed \n");
+        return -1;
+    }
 
-    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+    // Send name to server
+    // send(sock, buffer, strlen(buffer), 0);
+    // Receive greeting message from server
+    read(sock, buffer, BUFFER_SIZE);
+    printf("%s\n", buffer);
+
+    snprintf(buffer, sizeof(buffer), "%s", bye_mes);
+    send(sock, buffer, strlen(buffer), 0);
+    printf("%s\n", bye_mes);
+    close(sock);
+}
+int main()
+{
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *firstname = "Enter your firstname: ";
+    char *lastname = "Enter your lastname: ";
+    char *birthday = "Enter your birthday in format <ddmmyyyy>: ";
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF)
-        {
-            printf("\n Error : Fputs error\n");
-        }
-    } 
+        printf("\n Socket creation error \n");
+        return -1;
+    }
 
-    if(n < 0)
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
     {
-        printf("\n Read error \n");
-    } 
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    printf("%s", firstname);
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
+
+    // Send name to server
+    send(sock, buffer, strlen(buffer), 0);
+
+    // Receive greeting message from server
+    read(sock, buffer, BUFFER_SIZE);
+    printf("%s\n", buffer);
+
+    printf("%s", lastname);
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
+
+    // Send name to server
+    send(sock, buffer, strlen(buffer), 0);
+
+    // Receive greeting message from server
+    read(sock, buffer, BUFFER_SIZE);
+    printf("%s\n", buffer);
+
+    printf("%s", birthday);
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
+
+    // Send name to server
+    send(sock, buffer, strlen(buffer), 0);
+
+    // Receive greeting message from server
+    read(sock, buffer, BUFFER_SIZE);
+    printf("%s\n", buffer);
+    close(sock);
+    port2();
+    
     return 0;
 }
--- hw4/server.c
+++ hw4/server.c
@@ -1,43 +1,192 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <time.h> 
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[])
+#define PORT 12056
+#define PORT2 22056
+#define BUFFER_SIZE 1024
+
+int	getlen 	(int num)
 {
-    int listenfd = 0, connfd = 0;
-    struct sockaddr_in serv_addr; 
+	int len = 0;
 
-    char sendBuff[1025];
-    time_t ticks; 
+	if (num < 0)
+		len++;
+	while (num != 0)
+	{
+		len++;
+		num /= 10;
+	}
+	return (len);
+}
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff)); 
+char	*ft_itoa(int nbr)
+{
+	char	*res;
+	int	len;
+	long	num = nbr;
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000); 
+	len = getlen(nbr);
+	res = malloc(sizeof(char) * (len + 1));
+	res[len] = '\0';
 
-    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+	if (num < 0)
+		res[0] = '-';
+	else if (num == 0)
+		res[0] = '0';
 
-    listen(listenfd, 10); 
-
-    while(1)
+	while (num != 0)
+	{
+		--len;
+		if (num < 0)
+			num *= -1;
+		res[len] = (num%10) + '0';
+		num /= 10;
+	}
+	return (res);
+}
+void port2(char *buffer)
+{
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char *recieved_mes = "Bye";
+    char response[BUFFER_SIZE];
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
-        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-        ticks = time(NULL);
-        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-        write(connfd, sendBuff, strlen(sendBuff)); 
+    // Bind the socket to the network address and port
+    bzero((char *)&address, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT2);
 
-        close(connfd);
-        sleep(1);
-     }
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT2);
+
+    // Accept incoming connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // printf("time is %d", time);
+
+    // Read client's name
+    //read(new_socket, buffer, BUFFER_SIZE);
+    printf("Year is : %s\n", buffer);
+
+    // Send recieved_mes message to client
+    snprintf(response, sizeof(response), "%s", buffer);
+    send(new_socket, response, strlen(response), 0);
+
+    bzero(buffer, BUFFER_SIZE);
+    read(new_socket, buffer, BUFFER_SIZE);
+    printf("Client's response: %s\n", buffer);
+    close(new_socket);
+    close(server_fd);
+}
+int main()
+{
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int year = 0;
+    int time = 3;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    char *recieved_mes = "Server received";
+    char response[BUFFER_SIZE];
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind the socket to the network address and port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    // Accept incoming connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    while (time > 0)
+    {
+        // printf("time is %d", time);
+
+        // Read client's name
+        read(new_socket, buffer, BUFFER_SIZE);
+        printf("Client's data: %s\n", buffer);
+
+        // Send recieved_mes message to client
+        snprintf(response, sizeof(response), "%s", recieved_mes);
+        send(new_socket, response, strlen(response), 0);
+        if (time > 1)
+            bzero(buffer, BUFFER_SIZE);
+        time--;
+    }
+    close(new_socket);
+    close(server_fd);
+
+    year = atoi(buffer);
+    year%=10000;
+    year-=543;
+    char *str_year = ft_itoa(year);
+    // printf("%s", str_year);
+    int i = 0;
+    while (i < 4)
+    {
+        buffer[i] = str_year[i];
+        // printf("<%c>", buffer[i]);
+        i++;
+    }
+    buffer[i] = '\0';
+    port2(buffer);
+
+    return 0;
 }
